4e74baebf216d76ee57d2ba0c268a472797327aa
João Almeida <jvastos@gmail.com>
2022-06-07 15:27:49 +0200

    alert for username implemented

--- frontend/src/components/SearchBlock/GoButton.jsx
+++ frontend/src/components/SearchBlock/GoButton.jsx
@@ -8,17 +8,25 @@ function GoButton(props) {
     const setRepoToScan = props.setRepoToScan;
 
     //Function to fetch the public repos of the given username.
-    //It also decides if the result box should be displayed on the interface.
+    //It also decides if the repo fetching process should be started and if the result box should be displayed on the interface.
     //(because if a new user is given through the input field, we want to hide the result from the last clicked repo of the previous given user.)
     async function getRepos() {
         const inputValue = document.getElementById('input').value;
         console.log(inputValue);
-        if (inputValue !== userName) {
+        if (!inputValue) {
+            alert('Please enter username.');
+        } else if (inputValue !== userName) {
             setUserName(inputValue);
             const repos = await fetchRepos(inputValue);
             setRepos(repos);
             setRepoToScan(); //this makes the result box disappear on the interface.
         }
+        /* if (inputValue !== userName) {
+            setUserName(inputValue);
+            const repos = await fetchRepos(inputValue);
+            setRepos(repos);
+            setRepoToScan(); //this makes the result box disappear on the interface.
+        } */
     }
 
     return <button onClick={getRepos}>Go</button>;
